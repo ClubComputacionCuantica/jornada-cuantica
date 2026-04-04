81b4dd532ef5700f5c5c01a5cd2c7d0da041f847
RobBEN93 <robnavar84@gmail.com>
2026-04-04 00:47:03 -0600

    Update event page

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -12,26 +12,32 @@ var store = [{
         "teaser": null
       },{
     "title": "Welcome!",
-    "excerpt":"Welcome   Hello world, this is a post test.  ","url": "https://clubcomputacioncuantica.github.io/assets/2025-02-16-post_test/"
+    "excerpt":"Welcome   Hello world, this is a post test.  ","url": "http://localhost:4000/assets/2025-02-16-post_test/"
   },{
     "title": "Page Not Found",
-    "excerpt":"Lo sentimos, pero la página que intentabas ver no existe…  ","url": "https://clubcomputacioncuantica.github.io/404.html"
+    "excerpt":"Lo sentimos, pero la página que intentabas ver no existe…  ","url": "http://localhost:4000/404.html"
   },{
     "title": "Temario Taller: Física Emergente",
-    "excerpt":"Temario 1. Introducción a la Computación Cuántica 1.1 ¿Qué es un qubit? ¿Qué es una computadora cuántica? Exploraremos los conceptos fundamentales de la computación cuántica, comenzando con el qubit y cómo se diferencia de los bits clásicos, así como las bases de una computadora cuántica. 1.2 Complejidad Big-O Estudiaremos la...","url": "https://clubcomputacioncuantica.github.io/temario-taller-fisica-frontera"
+    "excerpt":"Temario 1. Introducción a la Computación Cuántica 1.1 ¿Qué es un qubit? ¿Qué es una computadora cuántica? Exploraremos los conceptos fundamentales de la computación cuántica, comenzando con el qubit y cómo se diferencia de los bits clásicos, así como las bases de una computadora cuántica. 1.2 Complejidad Big-O Estudiaremos la...","url": "http://localhost:4000/temario-taller-fisica-frontera"
+  },{
+    "title": "Sitio en mantenimiento",
+    "excerpt":"\r   Volveremos pronto \r ","url": "http://localhost:4000/"
+  },{
+    "title": "Jornada de Investigación y Divulgación en Información y Cómputo Cuántico",
+    "excerpt":"Les damos la bienvenida a la Jornada de Investigación y Divulgación en Información y Cómputo Cuántico, un espacio académico orientado a la presentación, discusión y articulación de investigaciones en el área. Dirigido a investigadores, estudiantes de posgrado, estudiantes avanzados de licenciatura, grupos de investigación emergentes y público interesado en información...","url": "http://localhost:4000/jornada/"
   },{
     "title": "Organizadores",
-    "excerpt":"### 👥 Organizadores del Club Nuestro club es liderado por un estudiantes apasionados por la **computación cuántica**, dedicados a crear un espacio de aprendizaje, discusión y colaboración. - **Rob Navarro** – Coordinador del club ⟡ Enfocado en el estudio de redes cuánticas y computación cuántica. Coordina el club promoviendo un...","url": "https://clubcomputacioncuantica.github.io/organizadores"
+    "excerpt":"### 👥 Organizadores del Club Nuestro club es liderado por un estudiantes apasionados por la **computación cuántica**, dedicados a crear un espacio de aprendizaje, discusión y colaboración. - **Rob Navarro** – Coordinador del club ⟡ Enfocado en el estudio de redes cuánticas y computación cuántica. Coordina el club promoviendo un...","url": "http://localhost:4000/organizadores"
   },{
     "title": "Sobre Nosotros",
-    "excerpt":"### 🏛️ **Quiénes somos** Somos una comunidad comprometida con la exploración y difusión de esta tecnología en auge. Reuniumos estudiantes, investigadores y entusiastas que buscan aprender, colaborar y experimentar. #### [Conoce a los organizadores](/organizadores) ### 🌍 ¿A quién va dirigido? Este club está abierto a cualquier persona interesada en la...","url": "https://clubcomputacioncuantica.github.io/sobre-nosotros"
+    "excerpt":"### 🏛️ **Quiénes somos** Somos una comunidad comprometida con la exploración y difusión de esta tecnología en auge. Reuniumos estudiantes, investigadores y entusiastas que buscan aprender, colaborar y experimentar. #### [Conoce a los organizadores](/organizadores) ### 🌍 ¿A quién va dirigido? Este club está abierto a cualquier persona interesada en la...","url": "http://localhost:4000/sobre-nosotros"
   },{
     "title": "Sudoku NISQ",
-    "excerpt":"### 🚀 Coming Soon... Can **Noisy Intermediate-Scale Quantum (NISQ) computing** solve **Sudoku puzzles**? This project will put quantum algorithms and current NISQ hardware's capabilities to the test on a setting that's accessible to anyone. 🔬 **What to Expect:** - **Quantum & Hybrid Approaches** 🧠⚛️ - **Benchmarking NISQ Software & Hardware**...","url": "https://clubcomputacioncuantica.github.io/sudoku-nisq-en"
+    "excerpt":"### 🚀 Coming Soon... Can **Noisy Intermediate-Scale Quantum (NISQ) computing** solve **Sudoku puzzles**? This project will put quantum algorithms and current NISQ hardware's capabilities to the test on a setting that's accessible to anyone. 🔬 **What to Expect:** - **Quantum & Hybrid Approaches** 🧠⚛️ - **Benchmarking NISQ Software & Hardware**...","url": "http://localhost:4000/sudoku-nisq-en"
   },{
     "title": "Sudoku NISQ",
-    "excerpt":"🔗 [English version](/sudoku-nisq-en) ### 🚀 Próximamente... ¿Puede la **computación cuántica de la era NISQ (Noisy Intermediate-Scale Quantum)** resolver **sudokus**? Este proyecto pone a prueba los algoritmos cuánticos y las capacidades del hardware NISQ actual en un problema accesible para todos. 🔬 **Qué esperar:** - **Enfoques cuánticos e híbridos** 🧠⚛️ -...","url": "https://clubcomputacioncuantica.github.io/sudoku-nisq"
+    "excerpt":"🔗 [English version](/sudoku-nisq-en) ### 🚀 Próximamente... ¿Puede la **computación cuántica de la era NISQ (Noisy Intermediate-Scale Quantum)** resolver **sudokus**? Este proyecto pone a prueba los algoritmos cuánticos y las capacidades del hardware NISQ actual en un problema accesible para todos. 🔬 **Qué esperar:** - **Enfoques cuánticos e híbridos** 🧠⚛️ -...","url": "http://localhost:4000/sudoku-nisq"
   },{
     "title": "Talleres",
-    "excerpt":"","url": "https://clubcomputacioncuantica.github.io/talleres/"
+    "excerpt":"","url": "http://localhost:4000/talleres/"
   }]
